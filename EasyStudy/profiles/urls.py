2abee23e0337b246d632c3dc9e9a4cb36ed24300
Andrey Seluykov <trezortop92@gmail.com>
2021-03-30 22:53:58 +0300

    profile list update

--- EasyStudy/profiles/urls.py
+++ EasyStudy/profiles/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
-from .views import my_profile_view, invites_received_view, profiles_list_view, invite_profiles_list_view
+from .views import (
+    my_profile_view,
+    invites_received_view,
+    profiles_list_view,
+    invite_profiles_list_view,
+    ProfileListView
+)
 
 app_name = 'profiles'
 
 urlpatterns = [
     path('myprofile/', my_profile_view, name='my_profile_view'),
     path('my_invites/', invites_received_view, name='my_invites_view'),
-    path('all_profiles/', profiles_list_view, name='all_profiles_view'),
+    path('all_profiles/', ProfileListView.as_view(), name='all_profiles_view'),
     path('to_invite/', invite_profiles_list_view, name='invite_profiles_list_view'),
 ]
--- EasyStudy/profiles/views.py
+++ EasyStudy/profiles/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Profile, Relationship
 from .forms import ProfileModelForm
+from django.views.generic import ListView
+from django.contrib.auth.models import User
 
 
 def my_profile_view(request):
@@ -31,6 +33,15 @@ def invites_received_view(request):
     return render(request, 'profiles/my_invites.html', context)
 
 
+def invite_profiles_list_view(request):
+    user = request.user
+    query_set = Profile.objects.get_all_profiles_to_invite(user)
+
+    context = {'query_set': query_set}
+
+    return render(request, 'profiles/to_invite.html', context)
+
+
 def profiles_list_view(request):
     user = request.user
     query_set = Profile.objects.get_all_profiles(user)
@@ -40,10 +51,37 @@ def profiles_list_view(request):
     return render(request, 'profiles/profile_list.html', context)
 
 
-def invite_profiles_list_view(request):
-    user = request.user
-    query_set = Profile.objects.get_all_profiles_to_invite(user)
+class ProfileListView(ListView):
+    model = Profile
+    template_name = 'profiles/profile_list.html'
 
-    context = {'query_set': query_set}
+    context_object_name = 'query_set'
 
-    return render(request, 'profiles/to_invite.html', context)
+    def get_queryset(self):
+        query_set = Profile.objects.get_all_profiles(self.request.user)
+        return query_set
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = User.objects.get(username__iexact=self.request.user)
+        profile = Profile.objects.get(user=user)
+        relation_receiver = Relationship.objects.filter(sender=profile)
+        relation_sender = Relationship.objects.filter(receiver=profile)
+
+        relation_receiver_listed = []
+        relation_sender_listed = []
+
+        for item in relation_receiver:
+            relation_receiver_listed.append(item.receiver.user)
+
+        for item in relation_sender:
+            relation_sender_listed.append(item.sender.user)
+
+        context["relation_receiver"] = relation_receiver_listed
+        context["relation_sender"] = relation_sender_listed
+        context['is_empty'] = False
+
+        if len(self.get_queryset()) == 0:
+            context['is_empty'] = True
+
+        return context
